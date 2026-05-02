015a705efc3b20e8507304bf0c214301c1dbb238
AricelCastro <158506241+AricelCastro@users.noreply.github.com>
2026-05-02 10:29:13 -0600

    frontend

--- backend/src/main/java/com/sistema/academico/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistema.academico.repository;
-
-import com.sistema.academico.entity.Alumno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
-}
